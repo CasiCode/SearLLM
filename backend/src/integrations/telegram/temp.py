86b978c2aeec2f133f2096610382ecb06f1941ca
casi <pitsunovtimur@gmail.com>
2025-07-01 23:23:35 +0400

    change temp.py

--- backend/src/integrations/telegram/temp.py
+++ backend/src/integrations/telegram/temp.py
@@ -6,6 +6,11 @@ from src.api.structs import OutputMessage
 # минимум кода, чтобы получить соответствующий этой модели json ответ от api:
 import requests
 
-response = requests.post("api-url/query", "запрос к api")
+query = {
+    "session_id": "abc123",
+    "message": "В каких областях Neoflex обладает экспертизой?",
+}
+response_json = requests.post("api-url/query", json=query)
+response = OutputMessage.model_validate_json(response_json)
 
 # можно пробовать получать ответ не через API, а напрямую от langgraph, но мне нужно подумать над тем, насколько это резонно
