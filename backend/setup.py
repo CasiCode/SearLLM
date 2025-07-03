e272be824f710e2218b08772efef6038778320fd
casi <pitsunovtimur@gmail.com>
2025-07-03 12:22:35 +0400

    change setup.py

--- backend/setup.py
+++ backend/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="backend",
     version="0.1.0",
-    packages=find_packages(where="backend"),
+    packages=find_packages(),
     package_dir={"": "src"},
     install_requires=[
         "langchain-community",
