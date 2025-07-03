d6395da077a507df13f6366a17022c9e2674a93a
casi <pitsunovtimur@gmail.com>
2025-07-03 23:47:01 +0400

    modify get_config to use absolute imports

--- backend/src/backend/utils.py
+++ backend/src/backend/utils.py
@@ -1,20 +1,25 @@
+import os
 import warnings
 
 import yaml
 from box import Box
 
 
+DIRNAME = os.path.dirname(__file__)
+
+
 def get_config(path: str) -> Box:
     """
-    Returns config as object
+    Returns config as object.
 
     arguments:
-        path: path to config.yml
+        path: path to config.yml, relative to backend/src/backend/.
     returns:
         Box object holding config
     """
+    abs_path = os.path.join(DIRNAME, path)
     try:
-        with open(path, "r") as f:
+        with open(abs_path, "r") as f:
             config_dict = yaml.safe_load(f)
         return Box(config_dict)
     except Exception as e:
