37b35058dd987396dc0326ee0ac428e1a4f37f17
casi <pitsunovtimur@gmail.com>
2025-07-03 12:08:48 +0400

    change .ignores, change setup.py, add temp.py for testing

--- backend/setup.py
+++ backend/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="backend",
+    name="src",
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
---
+++ backend/src/api/temp.py
@@ -0,0 +1,1 @@
+from backend.src.structs import SearchQueryList
