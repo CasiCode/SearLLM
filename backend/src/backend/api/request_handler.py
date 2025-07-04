96dbc9c211744ec785b8e5c0c4862be3edc6a6e9
casi <pitsunovtimur@gmail.com>
2025-07-04 11:54:40 +0400

    add warning comments

--- backend/src/backend/api/request_handler.py
+++ backend/src/backend/api/request_handler.py
@@ -12,12 +12,13 @@ class RequestHandler:
             raise ValueError("Process function must be callable")
         self._process_func = func
 
-    def process_request(self, session_id: str, question: str):
+    def process_request(self, session_id: str, input_message: str):
         if self._process_func is None:
             raise LocalAPIException(details="No process function registered on server.")
 
         try:
-            response = self._process_func(session_id, question)
+            # ! response RHS is not correct, no config, no anything
+            response = self._process_func(session_id, input_message)
             required_keys = {"answer", "source_documents", "session_id"}
             if not all(key in response for key in required_keys):
                 raise ExternalAPIException(details="Bad response format.")
