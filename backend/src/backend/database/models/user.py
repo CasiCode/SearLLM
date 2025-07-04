6f1dc54f23a85e19c32a4ca45efce7973a9a35f4
casi <pitsunovtimur@gmail.com>
2025-07-04 23:06:14 +0400

    add token limit

--- backend/src/backend/database/models/user.py
+++ backend/src/backend/database/models/user.py
@@ -8,6 +8,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True, nullable=False, unique=True)
     tokens_used = Column(Integer, default=0, nullable=False)
+    token_limit = Column(Integer, default=0, nullable=False)
 
     def __repr__(self):
         return f"<User(id={self.id}, tokens_used={self.tokens_used})>"
