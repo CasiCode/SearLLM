05131e0a3aaa57e2b0dd1b799599d0a37103c28f
casi <pitsunovtimur@gmail.com>
2025-07-05 01:53:27 +0400

    add security/utils.py

---
+++ backend/src/backend/security/utils.py
@@ -0,0 +1,11 @@
+import secrets
+import base64
+
+
+def generate_service_key(prefix: str = ""):
+    random_bytes = secrets.token_bytes(64)
+
+    service_key = base64.urlsafe_b64encode(random_bytes).decode("utf-8")
+    service_key = f"{prefix}_{service_key}"
+
+    return service_key
