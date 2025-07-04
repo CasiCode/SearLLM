e07fbef537b131cc3fc81d3a5904661e22daa578
casi <pitsunovtimur@gmail.com>
2025-07-04 23:06:46 +0400

    change module structure

--- backend/src/backend/api/app.py
+++ backend/src/backend/api/app.py
@@ -1,57 +1,9 @@
-from fastapi import Depends, FastAPI, Request
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI
 
-from backend.agent.graph import process_input_message
-from backend.api.exceptions import ExternalAPIException, LocalAPIException
-from backend.api.structs import InputMessage, OutputMessage, SourceDocument
-from backend.api.request_handler import RequestHandler
+from backend.api.core.exceptions import setup_exception_handlers
+from backend.api.routers import queries
 
 
 app = FastAPI()
-request_handler = RequestHandler()
-request_handler.set_process_function(process_input_message)
-
-
-def get_handler():
-    return request_handler
-
-
-@app.exception_handler(LocalAPIException)
-def local_api_exception_handler(request: Request, exc: LocalAPIException):
-    return JSONResponse(
-        status_code=exc.status_code,
-        content={"message": f"Oops! Local API did something... {exc.details}"},
-    )
-
-
-@app.exception_handler(ExternalAPIException)
-def external_api_exception_handler(request: Request, exc: ExternalAPIException):
-    status_code = exc.status_code
-    return JSONResponse(
-        status_code=status_code,
-        content={"message": f"Oops! External API did something... {exc.details}"},
-    )
-
-
-@app.post("/query", response_model=OutputMessage)
-async def ask_question(
-    input: InputMessage, handler: RequestHandler = Depends(get_handler)
-):
-    response = handler.process_request(input.session_id, input.message)
-    return OutputMessage(
-        message=response["message"],
-        source_documents=response["source_documents"],
-        session_id=response["session_id"],
-    )
-
-
-# ! DEVELOPMENT-ONLY ENDPOINT
-@app.post("/dev", response_model=OutputMessage)
-async def response(input: InputMessage):
-    return OutputMessage(
-        message="This is a dev message, yaaaay!",
-        source_documents=[
-            SourceDocument(source="this is a source", snippet="...some snippet...")
-        ],
-        session_id=input["session_id"],
-    )
+setup_exception_handlers(app)
+app.include_router(queries.router, prefix="/queries", tags=["queries"])
