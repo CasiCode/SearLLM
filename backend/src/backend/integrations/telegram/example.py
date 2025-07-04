75b54926fa54c7f92adcbfdaa10ba227a2a1ab9e
casi <pitsunovtimur@gmail.com>
2025-07-04 11:51:46 +0400

    rename temp.py to example.py

---
+++ backend/src/backend/integrations/telegram/example.py
@@ -0,0 +1,34 @@
+# Добавил этот файл только чтобы создать ноду на гите. Можешь свободно его удалять после того, как добавишь в папку что-то еще.
+
+# это pydantic модель выходного сообщения
+from backend.api.structs import OutputMessage
+
+# чтобы подгрузить конфиг
+from backend.utils import get_config
+
+config = get_config("api/config.yml")
+
+# конфигов может быть много по разным путям, но утилита подгружает ЛЮБОЙ
+# ты можешь написать и свой yml конфиг для своего модуля
+
+# минимум кода, чтобы получить соответствующий этой модели json ответ от api:
+import requests
+
+query = {
+    "session_id": "abc123",
+    "user_id": 123,
+    "message": "Какое второе имя у Барака Обамы?",
+}
+
+api_url = f"{config.api.host}:{str(config.api.port)}"
+response_json = requests.post(f"{api_url}/dev", json=query)
+response = OutputMessage.model_validate_json(response_json)
+
+
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+super_secret_api_key = os.getenv("TELEGRAM_BOT_API_KEY")
