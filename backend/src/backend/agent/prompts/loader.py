00c9c72a521d4dc34d69cd08564e54df23cfdf71
casi <pitsunovtimur@gmail.com>
2025-07-05 11:38:54 +0400

    change loader logic

--- backend/src/backend/agent/prompts/loader.py
+++ backend/src/backend/agent/prompts/loader.py
@@ -1,4 +1,8 @@
 import warnings
+import os
+
+
+DIRNAME = os.path.dirname(__file__)
 
 
 class PromptLoader:
@@ -9,8 +13,15 @@ class PromptLoader:
         return self._filename
 
     def load_prompt(self) -> str:
+        """
+        Returns prompt as string.
+
+        returns:
+            String read from the prompt file
+        """
+        abs_path = os.path.join(DIRNAME, self._filename)
         try:
-            with open(self._filename, "r", encoding="utf-8") as f:
+            with open(abs_path, "r", encoding="utf-8") as f:
                 return f.read()
         except OSError:
             warnings.warn(
