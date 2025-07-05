7f9fa815f9c49a1c8fe2926755731cc9ed23dbd0
casi <pitsunovtimur@gmail.com>
2025-07-05 12:36:19 +0400

    add question note

--- backend/src/backend/agent/state.py
+++ backend/src/backend/agent/state.py
@@ -10,7 +10,9 @@ class OverallState(TypedDict):
     messages: Annotated[list, add_messages]
     search_query: Annotated[list, operator.add]
     web_research_result: Annotated[list, operator.add]
-    sources_gathered: Annotated[list, operator.add]  # ? Used in a questionable way
+    sources_gathered: Annotated[
+        list, operator.add
+    ]  # ? Not sorted and verified in graph
     initial_search_query_count: int
     max_research_loops: int
     research_loops_count: int
