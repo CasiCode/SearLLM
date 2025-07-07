9b80c99afaf3cd1386c5826488ab848cd395ead7
casi <pitsunovtimur@gmail.com>
2025-07-07 13:29:32 +0400

    change example

--- backend/src/backend/integrations/telegram/example.py
+++ backend/src/backend/integrations/telegram/example.py
@@ -1,18 +1,26 @@
 # Добавил этот файл только чтобы создать ноду на гите. Можешь свободно его удалять после того, как добавишь в папку что-то еще.
 
 # это pydantic модель выходного сообщения
-from backend.api.structs import OutputMessage
+from backend.api.core.structs import OutputMessage
 
-# чтобы подгрузить конфиг
+# чтобы подгрузить конфиг используй эту утилиту
 from backend.utils import get_config
-
-config = get_config("api/config.yml")
-
 # конфигов может быть много по разным путям, но утилита подгружает ЛЮБОЙ
 # ты можешь написать и свой yml конфиг для своего модуля
 
 # минимум кода, чтобы получить соответствующий этой модели json ответ от api:
-import requests
+from dotenv import load_dotenv
+import os
+
+from backend.api.client import APIClient
+
+load_dotenv()
+API_KEY = os.getenv("SEARXTG_API_KEY")
+
+config = get_config("api/config.yml")
+api_url = f"{config.api.host}:{str(config.api.port)}"
+
+client = APIClient(base_url=api_url, api_key=API_KEY)
 
 query = {
     "session_id": "abc123",
@@ -20,15 +28,8 @@ query = {
     "message": "Какое второе имя у Барака Обамы?",
 }
 
-api_url = f"{config.api.host}:{str(config.api.port)}"
-response_json = requests.post(f"{api_url}/dev", json=query)
-response = OutputMessage.model_validate_json(response_json)
 
-
-from dotenv import load_dotenv
-import os
-
-
-load_dotenv()
-
-super_secret_api_key = os.getenv("TELEGRAM_BOT_API_KEY")
+# функция, в которой совершается запрос к API должна быть асинхронной
+async def main():
+    response_json = await client.post(f"{api_url}/dev", json=query)
+    return OutputMessage.model_validate_json(response_json)
