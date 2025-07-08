22d79c52e1471308f7a9d847562b91f3bf791112
casi <pitsunovtimur@gmail.com>
2025-07-08 22:39:03 +0400

    add create classmethod, change fields

--- backend/src/backend/database/models/query.py
+++ backend/src/backend/database/models/query.py
@@ -1,6 +1,7 @@
 import datetime
 
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import Session
 
 from backend.database.base import Base
 
@@ -9,13 +10,19 @@ class Query(Base):
     __tablename__ = "queries"
 
     id = Column(Integer, primary_key=True, index=True, nullable=False, unique=True)
-    user_id = Column(
-        Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False, index=True
-    )
     made_at = Column(DateTime, default=datetime.datetime.now(), nullable=False)
 
-    tokens_used = Column(Integer, default=0, nullable=False)
     text = Column(String, default="", nullable=False)
 
     def __repr__(self):
-        return f"<Query(id={self.id}, user_id={self.user_id}, made_at={self.made_at}, tokens_used={self.tokens_used}, text={self.text})>"
+        return f"<Query(id={self.id}, made_at={self.made_at}, tokens_used={self.tokens_used}, text={self.text})>"
+
+    @classmethod
+    def create(
+        cls, db: Session, text: str
+    ):  # TODO: Calculate optimal ammount, add to config
+        new_query = cls(text=text)
+        db.add(new_query)
+        db.commit()
+        db.refresh(new_query)
+        return new_query
