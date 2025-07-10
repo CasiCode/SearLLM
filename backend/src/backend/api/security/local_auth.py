a9f756d78ea8118efe5498ece259e81ae6189919
casi <pitsunovtimur@gmail.com>
2025-07-10 14:55:59 +0400

    fix typo

--- backend/src/backend/api/security/local_auth.py
+++ backend/src/backend/api/security/local_auth.py
@@ -2,12 +2,12 @@ from fastapi import Request
 from fastapi.security import APIKeyHeader
 
 from backend.api.core.exceptions import InvalidKeyException
-from backend.security.utils import generate_service_key
+from backend.security.utils import generate_service_token
 
 
 class LocalAuth:
     def __init__(self):
-        self.token = generate_service_key(prefix="st")
+        self.token = generate_service_token(prefix="st")
         self.header_name = APIKeyHeader(name="SearXTG-API-token")
 
     def verify_token(self, request: Request):
