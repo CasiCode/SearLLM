8b84c7db39e78ed13583cba1aca47d1828c8214a
casi <pitsunovtimur@gmail.com>
2025-07-10 14:33:37 +0400

    naming change

--- backend/src/backend/security/utils.py
+++ backend/src/backend/security/utils.py
@@ -2,7 +2,7 @@ import secrets
 import base64
 
 
-def generate_service_key(prefix: str = ""):
+def generate_service_token(prefix: str = ""):
     random_bytes = secrets.token_bytes(64)
 
     service_key = base64.urlsafe_b64encode(random_bytes).decode("utf-8")
