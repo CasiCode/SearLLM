4a779351a5a3f5645e23b04e9a3c6fd7e6756c24
casi <pitsunovtimur@gmail.com>
2025-07-10 16:07:24 +0400

    add RuntimeAuth class

---
+++ backend/src/backend/api/security/runtime_auth.py
@@ -0,0 +1,39 @@
+import os
+from typing import Optional, Dict
+
+from dotenv import load_dotenv
+from fastapi import Depends
+from fastapi.security import APIKeyHeader
+
+from backend.api.core.exceptions import InvalidKeyException
+from backend.security.utils import generate_service_token
+
+load_dotenv()
+
+
+TOKEN_HEADER_NAME = os.getenv("API_TOKEN_HEADER", "SearXTG-API-token")
+token_header = APIKeyHeader(name=TOKEN_HEADER_NAME, auto_error=False)
+
+
+class RuntimeAuth:
+    def __init__(self):
+        self.__token = generate_service_token(prefix="st")
+
+    def get_token(self) -> str:
+        return self.__token
+
+    def verify_token(self, token: Optional[str] = None):
+        if not token or self.__token != token:
+            raise InvalidKeyException("Invalid API token used.")
+        return token
+
+
+auth = RuntimeAuth()
+
+
+def require_api_token(token: str = Depends(token_header)):
+    return auth.verify_token(token)
+
+
+def get_service_token_header() -> Dict[str, str]:
+    return {TOKEN_HEADER_NAME: auth.get_token()}
