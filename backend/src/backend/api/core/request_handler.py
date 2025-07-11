0eaa3d27a8a5179c79db8e15779f64af26f7a332
casi <pitsunovtimur@gmail.com>
2025-07-11 19:39:20 +0400

    add docstrings

--- backend/src/backend/api/core/request_handler.py
+++ backend/src/backend/api/core/request_handler.py
@@ -1,3 +1,5 @@
+"""Manages API requests and transmits them to the Langgraph layer"""
+
 from typing import Callable, Optional
 
 from langchain_core.runnables import RunnableConfig
@@ -6,10 +8,33 @@ from backend.api.core.exceptions import ExternalAPIException, LocalAPIException
 
 
 class RequestHandler:
+    """RequestHandler
+
+    Methods
+    -------
+    __init__(self):
+        Constructor
+    set_process_function(func: Callable):
+        Sets self._process_func to func.
+    process_request(session_id: str, user_id: int, input_message: str, config: RunnableConfig):
+        Processes incoming request and tries to run self._process_func on its data
+    """
+
     def __init__(self):
+        """
+        Constructor
+        """
+
         self._process_func: Optional[Callable] = None
 
     def set_process_function(self, func: Callable):
+        """
+        Sets self._process_func to func.
+
+        Parameters:
+            func (Callable): function to be set as a request processor
+        """
+
         if not callable(func):
             raise ValueError("Process function must be callable")
         self._process_func = func
@@ -17,6 +42,16 @@ class RequestHandler:
     def process_request(
         self, session_id: str, user_id: int, input_message: str, config: RunnableConfig
     ):
+        """
+        Processes incoming request and tries to run self._process_func on its data
+
+        Parameters:
+            session_id (str): Request session id
+            user_id (int): id of the user who issued the request
+            input_message (str): user message to be processed with langgraph
+            config (RunnableConfig): langgraph config
+        """
+
         if self._process_func is None:
             raise LocalAPIException(details="No process function registered on server.")
 
@@ -27,4 +62,4 @@ class RequestHandler:
                 raise ExternalAPIException(details="Bad response format.")
             return response
         except Exception as e:
-            raise ExternalAPIException(f"Unexpected error: {str(e)}")
+            raise ExternalAPIException(f"Unexpected error: {str(e)}") from e
