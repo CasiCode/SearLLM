1b36ab8b2b6216f51fb15bf06f7918783f24b656
casi <pitsunovtimur@gmail.com>
2025-07-11 18:39:43 +0400

    add docstrings

--- backend/src/backend/agent/configuration.py
+++ backend/src/backend/agent/configuration.py
@@ -1,12 +1,36 @@
+"""Configuration class used as a graph config."""
+
 import os
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 from langchain_core.runnables import RunnableConfig
 from pydantic import BaseModel, Field
-from typing import Optional
 
 
 class Configuration(BaseModel):
+    """Configuration
+
+    Attributes
+    ----------
+    model_adress : str
+        Full LLM adress as given by the model provider
+    model_name : str
+        General model name
+    temperature : float
+        Model temperature
+    number_of_initial_queries : int
+        Number of queries generated in the first graph node
+    max_research_loops : int
+        The maximum number of self-reflection loops the graph can go through
+
+    Methods
+    -------
+    from_runnable_config(config: Optional[RunnableConfig]):
+        Imports RunnableConfig instance into the instance of Configuration,
+        sets environmental variables as default
+
+    """
+
     model_adress: str = Field(default="openai/gpt-4.1-nano")
     model_name: str = Field(default="gpt-4.1-nano")
 
