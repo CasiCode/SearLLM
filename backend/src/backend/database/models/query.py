eea9aef8c0fe5ac1b0550b201a8b73faf4bd99ec
casi <pitsunovtimur@gmail.com>
2025-07-13 02:09:10 +0400

    move to utc datetime, fix repr method, delete todo

--- backend/src/backend/database/models/query.py
+++ backend/src/backend/database/models/query.py
@@ -10,17 +10,17 @@ class Query(Base):
     __tablename__ = "queries"
 
     id = Column(Integer, primary_key=True, index=True, nullable=False, unique=True)
-    made_at = Column(DateTime, default=datetime.datetime.now(), nullable=False)
+    made_at = Column(
+        DateTime, default=datetime.datetime.now(datetime.timezone.utc), nullable=False
+    )
 
     text = Column(String, default="", nullable=False)
 
     def __repr__(self):
-        return f"<Query(id={self.id}, made_at={self.made_at}, tokens_used={self.tokens_used}, text={self.text})>"
+        return f"<Query(id={self.id}, made_at={self.made_at}, text={self.text})>"
 
     @classmethod
-    def create(
-        cls, db: Session, text: str
-    ):  # TODO: Calculate optimal ammount, add to config
+    def create(cls, db: Session, text: str):
         new_query = cls(text=text)
         db.add(new_query)
         db.commit()
