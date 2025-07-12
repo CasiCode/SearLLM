7e626a2c25633fd90a3a21a5b1e993f7bfff0bce
casi <pitsunovtimur@gmail.com>
2025-07-12 16:24:01 +0400

    rewrite example

--- backend/src/backend/integrations/telegram/example.py
+++ backend/src/backend/integrations/telegram/example.py
@@ -1,26 +1,16 @@
-# Добавил этот файл только чтобы создать ноду на гите. Можешь свободно его удалять после того, как добавишь в папку что-то еще.
-
-# это pydantic модель выходного сообщения
-from backend.api.core.structs import OutputMessage
-
-# чтобы подгрузить конфиг используй эту утилиту
-from backend.utils import get_config
-# конфигов может быть много по разным путям, но утилита подгружает ЛЮБОЙ
-# ты можешь написать и свой yml конфиг для своего модуля
-
-# минимум кода, чтобы получить соответствующий этой модели json ответ от api:
-from dotenv import load_dotenv
 import os
 
+from dotenv import load_dotenv
+
 from backend.api.client import APIClient
+from backend.api.core.structs import OutputMessage
+from backend.utils import get_config
 
 load_dotenv()
 API_KEY = os.getenv("SEARXTG_API_KEY")
 
 config = get_config("api/config.yml")
-api_url = f"{config.api.host}:{str(config.api.port)}"
-
-client = APIClient(base_url=api_url, api_key=API_KEY)
+API_URL = f"{config.api.host}:{str(config.api.port)}"
 
 query = {
     "session_id": "abc123",
@@ -31,5 +21,6 @@ query = {
 
 # функция, в которой совершается запрос к API должна быть асинхронной
 async def main():
-    response_json = await client.post(f"{api_url}/dev", data=query)
-    return OutputMessage.model_validate_json(response_json)
+    async with APIClient(base_url=API_URL) as client:
+        response_json = await client.post("queries/dev", data=query)
+        print(OutputMessage(**response_json))
