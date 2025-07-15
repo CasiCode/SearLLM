b290ad1bfcd0127d7647c62ad1c794453fed2a20
casi <pitsunovtimur@gmail.com>
2025-07-15 14:26:58 +0400

    fix results number

--- backend/src/backend/agent/tools.py
+++ backend/src/backend/agent/tools.py
@@ -1,6 +1,5 @@
 """Agent tools"""
 
-import warnings
 from typing import Dict, List
 
 from langchain_community.utilities import SearxSearchWrapper
@@ -18,31 +17,15 @@ searx = SearxSearchWrapper(searx_host=SEARX_HOST)
 
 
 @tool(args_schema=SearchArgs)
-def search(query: str, num_results: int) -> List[Dict]:
+def search(query: str) -> List[Dict]:
     """
     Use this tool to get the results of a general web search.
 
     Args:
         query: The target query in a text format. This exact query will be passed to web-search.
-        num_results: The number of results to be returned by the tool.
 
     Returns:
         A list of dictionaries with these exact keys: snippet, title, link, engines, category
     """
-    if num_results < 1:
-        warnings.warn(
-            message=f"num_results set to {num_results}",
-            stacklevel=3,
-            category=RuntimeWarning,
-        )
-        num_results = 1
 
-    if num_results > 10:
-        warnings.warn(
-            message=f"num_results set to {num_results}",
-            stacklevel=3,
-            category=RuntimeWarning,
-        )
-        num_results = 10
-
-    return searx.results(query=query, num_results=num_results)
+    return searx.results(query=query, num_results=config.searx.num_results)
