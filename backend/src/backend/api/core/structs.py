2a449eaf9df8ab0ffebd256a1f5b83116b0e38e6
casi <pitsunovtimur@gmail.com>
2025-07-16 22:03:47 +0400

    migrate to list[str] from list[typeddict]

--- backend/src/backend/api/core/structs.py
+++ backend/src/backend/api/core/structs.py
@@ -7,14 +7,14 @@ class InputMessage(BaseModel):
     message: str
 
 
-class SourceDocument(BaseModel):
+class SourceDocument(BaseModel):  # ! Unused now, refactor the code to use list of str
     source: str
     snippet: str
 
 
 class OutputMessage(BaseModel):
     message: str
-    source_documents: list[SourceDocument]
+    source_documents: list[str]
     session_id: str
     input_tokens_used: int
     output_tokens_used: int
