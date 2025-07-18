5b1207e9767d50a1cdd027f7bd08ceb60f96c2b4
casi <pitsunovtimur@gmail.com>
2025-07-18 17:00:59 +0400

    change module docstring

--- backend/src/backend/api/__init__.py
+++ backend/src/backend/api/__init__.py
@@ -1,1 +1,1 @@
-"""A monolith core. Acts as a middleware FastAPI service which orchestrates all the other modules."""
+"""A monolith core. Acts as a middleware service which orchestrates all the other modules."""
