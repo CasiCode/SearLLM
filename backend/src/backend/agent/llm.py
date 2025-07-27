9064123278c2db5130b24a0abbbdbc6c3317d235
casi <pitsunovtimur@gmail.com>
2025-07-27 14:40:54 +0400

    add logging, add optional proxy

--- backend/src/backend/agent/llm.py
+++ backend/src/backend/agent/llm.py
@@ -1,6 +1,7 @@
 """Contains the logic for LLM's used in the graph"""
 
 import os
+import logging
 
 import httpx
 from dotenv import load_dotenv
@@ -11,6 +12,11 @@ from backend.agent.configuration import Configuration
 from backend.utils import check_env_variables
 
 load_dotenv()
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 
 def get_llm(config: RunnableConfig) -> ChatOpenAI:
@@ -25,13 +31,25 @@ def get_llm(config: RunnableConfig) -> ChatOpenAI:
     """
     configuration = Configuration.from_runnable_config(config)
 
-    check_env_variables(["OPENROUTER_API_KEY", "OPENROUTER_BASE_URL", "SAFE_PROXY_URL"])
+    check_env_variables(
+        [
+            "OPENROUTER_API_KEY",
+            "OPENROUTER_BASE_URL",
+        ]
+    )
+
+    try:
+        check_env_variables("SAFE_PROXY_URL")
+        proxy_client = httpx.Client(proxy=os.getenv("SAFE_PROXY_URL"))
+    except ValueError:
+        logger.warning(msg="SAFE_PROXY_URL not provided, falling back to direct HTTP")
+        proxy_client = None
 
     llm = ChatOpenAI(
         openai_api_key=os.getenv("OPENROUTER_API_KEY"),
         openai_api_base=os.getenv("OPENROUTER_BASE_URL"),
         model_name=configuration.model_adress,
         temperature=configuration.temperature,
-        http_client=httpx.Client(proxy=os.getenv("SAFE_PROXY_URL")),
+        http_client=proxy_client,
     )
     return llm
