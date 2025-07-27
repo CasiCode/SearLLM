245b1b010c2d3280d9cbd8dbb35a7beff4c64ff4
casi <pitsunovtimur@gmail.com>
2025-07-27 12:57:24 +0400

    add docstrings

--- backend/src/backend/api/core/structs.py
+++ backend/src/backend/api/core/structs.py
@@ -4,12 +4,44 @@ from pydantic import BaseModel
 
 
 class InputMessage(BaseModel):
+    """
+    Input message model
+
+    Attributes
+    ----------
+    session_id : str
+        unique session id used
+    user_id: int
+        unique user id
+    message: str
+        input message from the user
+    """
+
     session_id: str
     user_id: int
     message: str
 
 
 class OutputMessage(BaseModel):
+    """
+    Ouput message model
+
+    Attributes
+    ----------
+    message : str
+        full output message text
+    highlight : str
+        short highlight of message field
+    source_documents : list[str]
+        list of links used in the answer
+    session_id : str
+        unique session id
+    input_tokens_used : int
+        number of input tokens used for generation
+    output_tokens_used : int
+        number of output tokens used for generation
+    """
+
     message: str
     highlight: str
     source_documents: list[str]
