da41288952ea68eaae88dc707c89188a6cbd307e
casi <pitsunovtimur@gmail.com>
2025-07-27 12:57:57 +0400

    add docstrings

--- backend/src/backend/api/security/runtime_auth.py
+++ backend/src/backend/api/security/runtime_auth.py
@@ -18,14 +18,26 @@ token_header = APIKeyHeader(name=TOKEN_HEADER_NAME, auto_error=False)
 
 
 class RuntimeAuth:
+    """Runtime API authentification class. Manages service tokens.
+
+    Methods
+    -------
+    get_token():
+        returns the currently used service token
+    verify token(token: Optional[str]):
+        verifies if the used token matches with the true one
+    """
+
     def __init__(self):
         check_env_variables("SEARXTG_API_TOKEN")
         self.__token = os.getenv("SEARXTG_API_TOKEN")
 
     def get_token(self) -> str:
+        """Returns the currently used service token"""
         return self.__token
 
     def verify_token(self, token: Optional[str] = None):
+        """Verifies if the used token matches with the true one"""
         if not token or self.__token != token:
             raise InvalidKeyException("Invalid API token used.")
         return token
@@ -35,10 +47,12 @@ auth = RuntimeAuth()
 
 
 def require_api_token(token: str = Depends(token_header)):
+    """API token authentificatin dependency for FastAPI"""
     return auth.verify_token(token)
 
 
 # ? Not sure if this is safe
 def get_service_token_header() -> Dict[str, str]:
+    """Returns a service token header for HTTP auth"""
     check_env_variables("SEARXTG_API_TOKEN")
     return {TOKEN_HEADER_NAME: os.getenv("SEARXTG_API_TOKEN")}
